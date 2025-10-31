6fc49bbeffa9a7fa37a315640195456129e90c3c
nata <jtejerina51@gmail.com>
2025-10-30 21:59:38 -0300

    un pequeño avance con formulario

--- pelis/src/pages/Formulario.js
+++ pelis/src/pages/Formulario.js
@@ -1,1 +1,9 @@
-Wazaaaaa
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { NavLink, useNavigate } from 'react-router-dom';
+import './Formulario.css';
+
+Function Formulario() {
+
+    const {register, hadleSubmit, formState: {errors} }
+}
