7063715e272e1f0f2ebf1e262b29737792d54c63
nata <jtejerina51@gmail.com>
2025-11-01 21:54:51 -0300

    Formulario hecho

--- pelis/src/pages/DetallePelicula.js
+++ pelis/src/pages/DetallePelicula.js
@@ -1,1 +1,27 @@
-Wazaaaaa
+import React from 'react';
+import { useParams, NavLink } from 'react-router-dom';
+import './DetallePelicula.css';
+
+function DetallePelicula() {
+
+  const { id } = useParams();
+
+  const peliculas = {};
+
+  const pelicula = peliculas[id];
+
+  if (!pelicula) {
+    return (
+      <div className="container">
+        <h2>Película no encontrada</h2>
+        <p>No hay información disponible para esta película.</p>
+        <NavLink to="/" className="btn-volver">← Volver al Inicio</NavLink>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <NavLink to="/" className="btn-volver">
+        ← Volver al Inicio
+      </NavLink>
